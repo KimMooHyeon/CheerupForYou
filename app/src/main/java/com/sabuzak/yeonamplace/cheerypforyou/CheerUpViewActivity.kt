26c682ea3f1cfbf10ccba1d5551c2ab657a25eaa
KimMooHyeon <dlfb77@gmail.com>
2020-04-18 16:34:41 +0900

    Initial 응원해 Project

---
+++ app/src/main/java/com/sabuzak/yeonamplace/cheerypforyou/CheerUpViewActivity.kt
@@ -0,0 +1,12 @@
+package com.sabuzak.yeonamplace.cheerypforyou
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheerUpViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheer_up_view)
+    }
+}
