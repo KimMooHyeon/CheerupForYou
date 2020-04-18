4d77224091ed30ba806d4526e12036e19fa45d9b
choisunpil <roung4119@gmail.com>
2020-04-18 16:51:14 +0900

    Commit Test

--- app/src/main/java/com/sabuzak/yeonamplace/cheerypforyou/CheerUpViewActivity.kt
+++ app/src/main/java/com/sabuzak/yeonamplace/cheerypforyou/CheerUpViewActivity.kt
@@ -2,11 +2,14 @@ package com.sabuzak.yeonamplace.cheerypforyou
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class CheerUpViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheer_up_view)
+
+        Log.d("123","123")
     }
 }
