bbc42c07e1dc10fd9c98da1cd74b87e900f90d4a
choisunpil <roung4119@gmail.com>
2020-05-02 19:37:13 +0900

    삭제 버튼 구현중
    ##1. 이미지 리소스 비교
    ##2. 팝업 띄어서 비교
    남음

--- app/src/main/java/com/sabuzak/yeonamplace/cheerypforyou/MainActivity.kt
+++ app/src/main/java/com/sabuzak/yeonamplace/cheerypforyou/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.sabuzak.yeonamplace.cheerypforyou
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.voice.AlwaysOnHotwordDetector
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.annotation.NonNull
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
@@ -18,23 +23,39 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.activity_main.view.banner_recyclerview
 import kotlinx.android.synthetic.main.banner_recyclerview_item.view.*
+import kotlinx.coroutines.delay
+import org.jetbrains.anko.image
+import org.jetbrains.anko.imageResource
+import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.jetbrains.anko.startActivity
+import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     private var db:AppDatabase? =  null
+    private var isRemoveActive = false
     lateinit var mAdView : AdView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         btn_main_make_new_cheerup_text.setOnClickListener {
             startActivity<MakingCheerUpTextActivity>()
         }
-
-
-
         banner_recyclerview.adapter = BannerRecyclerViewAdapter()
         banner_recyclerview.layoutManager = LinearLayoutManager(this)
+        remove_button.setOnClickListener {
+            isRemoveActive = true
+           for(i in 0.. (banner_recyclerview.adapter as BannerRecyclerViewAdapter).itemCount)
+           {
+               (banner_recyclerview.adapter as BannerRecyclerViewAdapter).notifyItemChanged(i,"active")
+           }
+
+        }
+
+
 
         MobileAds.initialize(this) {}
         mAdView = adView
@@ -70,19 +91,70 @@ class MainActivity : AppCompatActivity() {
             return bannerArray.size
         }
 
+
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+
+        }
+
+        /*
+         *
+         */
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int,payloads: MutableList<Any>) {
             /**
              * Banner 객체 정의 후에 제대로 세팅 할것
              * 지금은 임시로 String 대체
              */
+            Log.d("payload",payloads.toString())
+            if(payloads.size > 0) {
+                if (payloads[0] == "active") {
+                    holder.itemView.delete_image.visibility = View.VISIBLE
+                } else if (payloads[0] == "disable") {
+                    holder.itemView.delete_image.visibility = View.GONE
+                }
+                else if(payloads[0] == "remove")
+                {
+                    holder.itemView.delete_image.setImageResource(R.drawable.ic_delete_red)
+                }
+            }
+            else{
 
-            holder.itemView.banner_text.setText(bannerArray[position])
+                holder.itemView.banner_text.setText(bannerArray[position])
+                holder.itemView.delete_image.onClick {
+
+
+
+                    //delay(1000)
+
+                    /**
+                     * TODO 2020.05.02 이미지 리소스 비교하여 빨강이랑 하얀색 구분
+                     */
+
+                    holder.itemView.delete_image.setImageResource(R.drawable.ic_delete_red)
+                    //holder.itemView.delete_image.visibility = View.GONE
+
+                        /**
+                         * TODO  2020.04팝업창 띄우기 해야됨
+                         */
+
+
+
+                }
+
+            }
+
+
         }
+
+
+
 
 
         inner class BannerViewHolder(view: View):RecyclerView.ViewHolder(view){
 
         }
+
+
 
     }
 }
