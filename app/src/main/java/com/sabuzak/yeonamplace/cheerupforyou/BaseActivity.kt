e9a9e9d8180a81948cecf9b715d30a91cdf573ed
KimMooHyeon <dlfb77@gmail.com>
2022-11-26 19:50:56 +0900

    chore : Databinding 적용을 위한 gradle 셋팅, BaseActivity 생성

---
+++ app/src/main/java/com/sabuzak/yeonamplace/cheerupforyou/BaseActivity.kt
@@ -0,0 +1,31 @@
+package com.sabuzak.yeonamplace.cheerupforyou
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(@LayoutRes private val layoutResId: Int) :
+    AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutResId)
+
+        overridePendingTransition(android.R.anim.fade_in, 0)
+
+        binding.lifecycleOwner = this
+
+        initView()
+    }
+
+    abstract fun initView()
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(0, android.R.anim.fade_out)
+    }
+}
