ecbc327b8d3bac910e10f42950bfc4c7a853efe2
KimMooHyeon <dlfb77@gmail.com>
2022-11-26 21:50:11 +0900

    refactor : Banner 삭제 팝업 화면 data binding 적용

--- app/src/main/java/com/sabuzak/yeonamplace/cheerupforyou/popup/DeleteBannerPopUpActivity.kt
+++ app/src/main/java/com/sabuzak/yeonamplace/cheerupforyou/popup/DeleteBannerPopUpActivity.kt
@@ -3,39 +3,31 @@ package com.sabuzak.yeonamplace.cheerupforyou.popup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.sabuzak.yeonamplace.cheerupforyou.BaseActivity
 import com.sabuzak.yeonamplace.cheerupforyou.dataBase.AppDatabase
-import com.sabuzak.yeonamplace.cheerupforyou.dataBase.Repository.BannerRepository
+import com.sabuzak.yeonamplace.cheerupforyou.dataBase.repository.BannerRepository
 import com.sabuzak.yeonamplace.cheerupforyou.R
+import com.sabuzak.yeonamplace.cheerupforyou.databinding.ActivityDeleteBannerPopUpBinding
 import kotlinx.coroutines.runBlocking
 
-class DeleteBannerPopUpActivity : AppCompatActivity() {
-    private lateinit var bannerRepository:BannerRepository
+class DeleteBannerPopUpActivity :
+    BaseActivity<ActivityDeleteBannerPopUpBinding>(R.layout.activity_delete_banner_pop_up) {
+    private lateinit var bannerRepository: BannerRepository
     override fun onBackPressed() {
         finish()
         this.overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
-
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_delete_banner_pop_up)
-        var tv_delete_confirm = findViewById<TextView>(R.id.tv_delete_confirm)
-        tv_delete_confirm.setOnClickListener {
 
-
-                var bannerIdx = intent.getIntExtra("bannerIdx",-1)
-                if (bannerIdx != -1)
-                {
-                    val bannerDao = AppDatabase.getDatabase(application).bannerDao()
-                    bannerRepository = BannerRepository(bannerDao)
-                        runBlocking {
-
-                            bannerRepository.deleteByIdx(bannerIdx)
-                        }
-
-
+    override fun initView() {
+        binding.tvDeleteConfirm.setOnClickListener {
+            val bannerIdx = intent.getIntExtra("bannerIdx", -1)
+            if (bannerIdx != -1) {
+                val bannerDao = AppDatabase.getDatabase(application).bannerDao()
+                bannerRepository = BannerRepository(bannerDao)
+                runBlocking {
+                    bannerRepository.deleteByIdx(bannerIdx)
                 }
-
-
+            }
             finish()
             this.overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
         }
