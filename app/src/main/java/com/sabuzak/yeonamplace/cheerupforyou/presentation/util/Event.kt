05dce1adc6a0da4b5ab66be2d6b541f84d588e44
KimMooHyeon <dlfb77@gmail.com>
2022-12-25 22:58:23 +0900

    issue#5 refactor : Home 화면 event 처리를 위한 HomeEvent 클래스 및 Event 래핑 클래스 생성

---
+++ app/src/main/java/com/sabuzak/yeonamplace/cheerupforyou/presentation/util/Event.kt
@@ -0,0 +1,16 @@
+package com.sabuzak.yeonamplace.cheerupforyou.presentation.util
+
+open class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+}
